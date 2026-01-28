5f056bea58382c376843226b99304492c38a395f
Syed Shaheer Khalid <shaheerkjaffer@gmail.com>
2026-01-28 09:40:09 +0500

    feat: added support for filters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -192,10 +192,15 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if !v.Valid() {
-		app.failedValidationResponse(w, r, v.Errors)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 		return
 	}
-	fmt.Fprintf(w, "%+v\n", input)
 
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
